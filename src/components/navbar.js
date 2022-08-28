91c3dcdee71c3e5b284f6378ce8df564129295d7
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2022-08-28 21:30:44 +0530

    Hamburger Implemented

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,39 +1,38 @@
-import React from "react";
-import Logo from '../assets/logo512.png';
-import '../css/navbar.css'
+import React, { useEffect, useState } from "react";
+import Logo from "../assets/logo512.png";
+import "../css/navbar.css";
 
-export default function navbar() {
+export default function Navbar() {
+  const [menu, setMenu] = useState(document.getElementById("menu"));
+
+  useEffect(() => {
+    setMenu(document.getElementById("menu"));
+  }, []);
+  
+  const updateMenu = ()=>{
+    if(menu.style.visibility === "visible") menu.style.visibility = "hidden";
+    else menu.style.visibility = "visible";
+  }
+
   return (
-    <navbar class="navbar bg-black m-0 p-0">
+    <div className="navbar bg-black m-0 p-0">
       <div className="logo">
-        <img class="m-2"
-          src={Logo}
-          width="40"
-          height="40"
-          alt="Logo"
-        />
-        <span class=" title align-middle">MovieZ</span>
+        <img className="m-2" src={Logo} width="40" height="40" alt="Logo" />
+        <span className="title">MovieZ</span>
       </div>
 
-      <div className="tabs">
+      <div className="tabs" id="menu">
         <div className="tab-button">Movies</div>
         <div className="tab-button">TV Shows</div>
         <div className="tab-button">Anime</div>
       </div>
       <div className="profile">
-        <Hamburger />
+        <div className="hamburger pe-2" id="ham-btn" onClick={updateMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
       </div>
-    </navbar>
-  );
-}
-
-
-function Hamburger(){
-  return(
-    <div className="hamburger pe-2">
-      <div className="line"></div>
-      <div className="line"></div>
-      <div className="line"></div>
     </div>
   );
 }
